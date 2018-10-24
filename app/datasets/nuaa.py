c63b744d69056861bee14d1215db47f3d8c941b2
Ryan Collins <ryan@ohmgeek.co.uk>
2018-10-24 21:27:23 +0100

    Add optimisation, add logging, add exception handling

--- app/datasets/nuaa.py
+++ app/datasets/nuaa.py
@@ -3,41 +3,58 @@ import numpy as np
 import cv2
 import glob
 import h5py
+from os.path import isfile as check_file_exists
+
+class PreprocessingIncompleteError(Exception):
+    """ An error to be raised when preprocessing hasn't been executed """
+    pass
 
 
 class NUAADataset(Dataset):
     _filename = ...  # type: str
 
-    def __init__(self, filename, labels=["ImposterRaw"]):
+    def __init__(self, logger, filename, labels=["ImposterRaw", "ClientRaw"]):
         """
         Initialise the dataset
         :param filename: the location of the extracted dataset on disk (the root folder).
         """
+        self._logger = logger
         self._filename = filename
         self._labels = labels
+        self._datasets = None
+        self._output_filename = '../datasets/nuaa/nuaa.h5'
         super().__init__()
 
     def pre_process(self):
         """Pre-process the dataset."""
         # First, load imposter images.
-        # Shape of image (N, max(H), max(W), channels).
-        # N: number of images, channels: number of channels in image e.g. 3 for RGB,
-        # H: height of all images. We use the maximium and we should probably mask.
-        # W: width for all images. We use max, but should probably mask.
-        print("start preprocess")
-        imposter_images = []
-        max_ctr = 0
-        for label in self._labels:
-            
-            for img_filename in glob.iglob(self._filename + '/%s/**/*.jpg' % label):
-                print(img_filename)
-                img = cv2.imread(img_filename)
-                imposter_images.append(img)
-                max_ctr += 1
+        self._datasets = []
 
-                # Break for testing TODO: Remove.
-                if max_ctr > 1000:
-                    break
-            with h5py.File('nuaa.h5', 'w') as hf:
-                dset = hf.create_dataset(label, data=imposter_images)
-                print("Shape Size:", dset.shape)
+        # If a training set exists, don't try to start again from scratch.
+        if check_file_exists(self._output_filename):
+            self._logger.info("File exists, so finish preprocessing early.")
+            return
+
+        with h5py.File(self._output_filename, 'w') as hf:
+
+            for label in self._labels:
+                self._logger.info("Start looking at label %s in dataset." % label)
+                label_images = []
+
+                # For each label, go through all the files in the directory/classification.
+                for img_filename in glob.iglob(self._filename + '/%s/**/*.jpg' % label):
+                    img = cv2.imread(img_filename)
+                    label_images.append(img)
+                # Now images are created and stored in the dataset.
+                dataset = hf.create_dataset(label, data=label_images)
+                self._logger.info("Dataset created")
+                self._datasets.append(dataset)
+
+        self._logger.info("Dataset preprocessing completed.")
+
+    def get_all_datasets(self):
+        if(self._datasets == None):
+            self._logger.error("Preprocessing wasn't executed. Therefore, no datasets are loaded.")
+            raise PreprocessingIncompleteError()
+        return self._datasets
+
--- app/test.py
+++ app/test.py
@@ -2,15 +2,15 @@ from datasets.nuaa import NUAADataset
 from testframework.runner import TestRunner
 import logging
 
-
 def main():
     # print("Running test.py")
     # logging.basicConfig(level=logging.INFO)
     # test_runner = TestRunner(logging.getLogger("root.TestRunner"), [], [], [])
     # test_runner.run()
-    dataset = NUAADataset("/home/ryan/datasets/nuaa")
+    dataset = NUAADataset(logging.getLogger("c.o.datasets.nuaa"), "/home/ryan/datasets/nuaa")
+    dataset.get_all_datasets()
     dataset.pre_process()
-
-
+    dataset.get_all_datasets()
+    
 if __name__ == "__main__":
     main()

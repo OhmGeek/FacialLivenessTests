1583dc23b6262cd4059545cc93a2ac7fbb99cf4e
Ryan Collins <ryan@ohmgeek.co.uk>
2018-10-24 12:11:59 +0100

    Add code that reads NUAA dataset

--- app/datasets/nuaa.py
+++ app/datasets/nuaa.py
@@ -2,6 +2,7 @@ from datasets.generic import Dataset
 import numpy as np
 import cv2
 import glob
+import h5py
 
 
 class NUAADataset(Dataset):
@@ -18,10 +19,22 @@ class NUAADataset(Dataset):
     def pre_process(self):
         """Pre-process the dataset."""
         # First, load imposter images.
-
+        # Shape of image (N, max(H), max(W), channels).
+        # N: number of images, channels: number of channels in image e.g. 3 for RGB,
+        # H: height of all images. We use the maximium and we should probably mask.
+        # W: width for all images. We use max, but should probably mask.
+        print("start preprocess")
         imposter_images = []
-
-        for img_filename in glob.iglob(self._filename + 'ImposterRaw/**/*.jpg'):
-            img = cv2.imread(img_filename, mode='RGB')
+        max_ctr = 0
+        for img_filename in glob.iglob(self._filename + '/ImposterRaw/**/*.jpg'):
+            print(img_filename)
+            img = cv2.imread(img_filename)
             imposter_images.append(img)
+            max_ctr += 1
 
+            # Break for testing TODO: Remove.
+            if max_ctr > 10:
+                break
+        with h5py.File('nuaa.h5', 'w') as hf:
+            dset = hf.create_dataset("imposter_images", data=imposter_images)
+            print("Shape Size:", dset.shape)

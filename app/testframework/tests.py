1936d016d2a2c0b84c6e7c17aaccc14a0838e783
Ryan Collins <ryan@ohmgeek.co.uk>
2018-10-30 20:28:20 +0000

    Tests

--- app/testframework/tests.py
+++ app/testframework/tests.py
@@ -1,5 +1,12 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from liveness.generic import AbstractLivenessTest
 
+class ModelNotValidError(Exception):
+    pass
+
+
+class TestDummyCase(GenericTest):
+        
 
 class GenericTest(ABC):
     """ A generic implementation of a test """
@@ -10,3 +17,14 @@ class GenericTest(ABC):
         :param logger: A logging object for a given test.
         """
         self._logger = logger
+
+    def run(self, model, data):
+        # First, make sure the given model is actually a liveness test. If not, break.
+        if not isinstance(model, typeof(AbstractLivenessTest)):
+            raise ModelNotValidError()
+
+        # get the model output
+        output = model.evaluate(data)
+
+        # return outcome from the model.
+        return output

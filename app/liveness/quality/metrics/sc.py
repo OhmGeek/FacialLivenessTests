3106c5db34e6e2bf44d422fa3a3e1a73b8f3efb2
Ryan Collins <ryan@ohmgeek.co.uk>
2018-11-04 21:42:50 +0000

    Added structural content

--- app/liveness/quality/metrics/sc.py
+++ app/liveness/quality/metrics/sc.py
@@ -1,3 +1,14 @@
 """
 Structural Content
 """
+import numpy as np
+
+from app.liveness.quality.metrics.generic import AbstractQualityMetric
+
+
+class StructuralContentMetric(AbstractQualityMetric):
+    def calculate(self, image, blurred_image):
+        image_squared_sum = np.squared(image).sum()
+        blurred_image_squared_sum = np.squared(blurred_image).sum()
+
+        return image_squared_sum / blurred_image_squared_sum

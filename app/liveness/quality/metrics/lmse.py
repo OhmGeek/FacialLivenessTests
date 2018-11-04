dc5aaf7db6b477d7dbae1ded628035f38bc18d7d
Ryan Collins <ryan@ohmgeek.co.uk>
2018-11-04 21:32:06 +0000

    Add nae, throw not implemented for laplacian

--- app/liveness/quality/metrics/lmse.py
+++ app/liveness/quality/metrics/lmse.py
@@ -2,3 +2,9 @@
 Laplacian Mean Squared Error metric.
 
 """
+from app.liveness.quality.metrics.generic import AbstractQualityMetric
+
+
+class LaplacianMeanSquaredMetric(AbstractQualityMetric):
+    def calculate(self, image, blurred_image):
+        raise NotImplementedError()

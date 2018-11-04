a32c2fe2fd0f2ea292279659da7ef79d9c11792a
Ryan Collins <ryan@ohmgeek.co.uk>
2018-11-04 21:36:12 +0000

    Implement Normalised cross correlation metric

--- app/liveness/quality/metrics/nxc.py
+++ app/liveness/quality/metrics/nxc.py
@@ -5,3 +5,20 @@ Simply the average difference I - G for all the pixels
 where I is the original image, and G is the gaussian blurred image.
 
 """
+import numpy as np
+
+from app.liveness.quality.metrics.generic import AbstractQualityMetric
+
+
+class NormalisedCrossCorrelationMetric(AbstractQualityMetric):
+    def calculate(self, image, blurred_image):
+        # First, find image dotted with blurred_image
+        dot_product = np.dot(image, blurred_image)
+
+        # Then square the components of image
+        image_squared = np.squared(image)
+
+        # Then do the division and return the result.
+        output = dot_product.sum() / image_squared.sum()
+
+        return output

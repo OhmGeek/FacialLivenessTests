90a101455a62b2c1e7d1e44276fea17e0b01e944
Ryan Collins <ryan@ohmgeek.co.uk>
2018-11-14 11:34:33 +0000

    Add Spectral Magnitude Error

---
+++ app/liveness/quality/metrics/sme.py
@@ -0,0 +1,30 @@
+"""
+Spectral Magnitude Error
+"""
+from metrics.generic import AbstractQualityMetric
+import numpy as np
+import cv2
+
+class SpectralMagnitudeErrorMetric(AbstractQualityMetric):
+    def calculate(self, image, blurred_image):
+        """
+        Calculate the spectral magnitude metric on an image and it's corresponding
+        gaussian blurred image
+        :param image: the full quality image
+        :param blurred_image: the gaussian blurred image
+        :return: the average difference value
+        """
+
+        # Run DFT. Two channels output: one being real, one being complex plane.
+        fourier_image = cv2.dft(np.float32(image), flags = cv2.DFT_COMPLEX_OUTPUT)
+        fourier_blurred_image = cv2.dft(np.float32(blurred_image), flags = cv2.DFT_COMPLEX_OUTPUT)
+        
+        image_dft_shift = np.fft.fftshift(fourier_image)
+        blurred_image_dft_shift = np.fft.fftshift(fourier_blurred_image)
+
+        fourier_mag_image = 20*np.log(cv2.magnitude(image_dft_shift[:,:,0],image_dft_shift[:,:,1]))
+        fourier_mag_blurred_image = 20*np.log(cv2.magnitude(blurred_image_dft_shift[:,:,0],blurred_image_dft_shift[:,:,1]))
+        
+        return np.mean(np.square(np.subtract(fourier_mag_image, fourier_mag_blurred_image)))
+        
+
--- app/liveness/quality/quality_tests.py
+++ app/liveness/quality/quality_tests.py
@@ -10,11 +10,12 @@ from metrics.ad import AverageDifferenceMetric
 from metrics.lmse import LaplacianMeanSquaredMetric
 from metrics.gme import GradientMagnitudeErrorMetric
 from metrics.gpe import GradientPhaseErrorMetric
+from metrics.sme import SpectralMagnitudeErrorMetric
 import cv2
 import logging
 def main():
     logger = logging.getLogger()
-    metrics = [GradientPhaseErrorMetric(logger)]
+    metrics = [SpectralMagnitudeErrorMetric(logger)]
 
     image = cv2.imread('/home/ryan/datasets/nuaa/ClientRaw/0001/0001_00_00_01_2.jpg')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)

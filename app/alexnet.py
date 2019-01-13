4ff1e7a1f5eae314c8fb642f318b89702561b8d8
Ryan Collins <ryan@ohmgeek.co.uk>
2019-01-13 15:21:18 +0000

    Alexnet works and trains reasonably well

--- app/alexnet.py
+++ app/alexnet.py
@@ -3,21 +3,22 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D, Lambda
 from keras.layers.normalization import BatchNormalization
+from keras.engine.input_layer import Input
 import numpy as np
 import cv2
 from datasets.nuaa import NUAADataset
+from keras.backend import tf
 
-def image_resize(img):
-    return cv2.resize(img, (224, 224))
 
 def create_model():
     # First, create an empty sequential model.
     model = Sequential()
 
+    # model.add(Input(shape=(None, None, 3)))
     # First, image resize layer (take any dimensional image down to 224 x 224 with 3 channels).
     # we do this by creating a lambda (through the predefined function above)
-    model.add(Lambda(image_size))
-
+    # model.add(Lambda(image_resize))
+    model.add(Lambda(lambda x: tf.image.resize_images(x,[224,224]), input_shape=(None,None,3)))
     # 1st Conv Layer
     model.add(Conv2D(filters=96, input_shape=(224,224,3), kernel_size=(11,11), strides=(4,4), padding='valid'))
     model.add(Activation('relu'))
@@ -93,26 +94,27 @@ def main():
     # Now create the CNN model
     model = create_model()
 
-    model.summary()
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.build()
+    model.summary()
 
     # Now create the training set.
     dataset = NUAADataset(logging.getLogger("c.o.datasets.nuaa"), "/home/ryan/datasets/nuaa/")
     dataset.pre_process()
 
     imposter_set = dataset.read_dataset("ImposterRaw")
-    imposter_y = np.repeat([1.0, 0.0], imposter_set.shape[0])
+    imposter_y = np.tile([1.0, 0.0], (imposter_set.shape[0], 1))
 
     client_set = dataset.read_dataset("ClientRaw")
-    client_y = np.repeat([0.0, 1.0], client_set.shape[0])
+    client_y = np.tile([0.0, 1.0], (client_set.shape[0], 1))
 
     # Merge the two, and create the final sets.
     x = np.concatenate((imposter_set, client_set))
     y = np.concatenate((imposter_y, client_y))
 
     # Train the model on our training set.
-    model.fit(x, y, batch_size=64, epochs=1, verbose=1, validation_split=0.2, shuffle=True)
+    model.fit(x, y, batch_size=64, epochs=5, verbose=1, validation_split=0.7, shuffle=True)
 
 if __name__ == "__main__":
     main()

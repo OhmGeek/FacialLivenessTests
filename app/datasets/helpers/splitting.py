cc582cafbc201ce50a08790c6f50f0241d874d5f
Ryan Collins <ryan@ohmgeek.co.uk>
2019-01-23 11:06:13 +0000

    splitting helper class

--- app/datasets/helpers/splitting.py
+++ app/datasets/helpers/splitting.py
@@ -1,17 +1,47 @@
+import cv2
+
+
 class DataSplitter:
-    def __init__(self):
-        pass
+    def __init__(self, logger, vector_creator):
+        self._logger = logger
+        self._vector_creator = vector_creator
 
-    def split(self, dataset, percentage_divisions=None):
+    def split(self, fake_set, real_set, percentage_train):
         """
-        Divide up a specified dataset into the specified percentages.
-        :param dataset:
-        :param percentage_divisions: list of the percentages to divide
-        :return: A len(percentage_divisions) tuple containing the divided data.
+        Take two tables of data, split them both by percentage, and return the result
+        :param fake_set: The spoofed image set
+        :param real_set: The real image set
+        :param percentage_train: The percentage of data that should be training data
+        :returns (train_vectors, train_outputs, test_vectors, test_outputs)
         """
-        # First, use the default of returning the entire dataset (no split).
-        if percentage_divisions is None:
-            percentage_divisions = [100]
-        pass
+        train_vectors = []
+        train_outputs = []
 
+        for imposter_img in fake_set:
+            try:
+                image = cv2.cvtColor(imposter_img, cv2.COLOR_BGR2GRAY)
+                gaussian_image = cv2.GaussianBlur(image, (5, 5), 0)
+                vector = self._vector_creator.create_vector(image, gaussian_image)
+                train_vectors.append(vector)
+                train_outputs.append(1.0)
+            except:
+                self._logger.error("Error while evaluating image.")
 
+        for real_img in real_set:
+            try:
+                image = cv2.cvtColor(real_img, cv2.COLOR_BGR2GRAY)
+                gaussian_image = cv2.GaussianBlur(image, (5, 5), 0)
+                vector = self._vector_creator.create_vector(image, gaussian_image)
+                train_vectors.append(vector)
+                train_outputs.append(0.0)
+            except:
+                self._logger.error("Error while evaluating image.")
+
+        n = len(train_outputs)
+
+        return (
+            train_vectors[:int(percentage_train * n)],
+            train_outputs[:int(percentage_train * n)],
+            train_vectors[int(percentage_train * n) + 1:],
+            train_outputs[int(percentage_train * n) + 1:]
+        )

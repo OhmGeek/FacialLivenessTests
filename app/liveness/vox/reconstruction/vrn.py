a4147762b024629d566bb7ca5ea3aa4f5fa57524
Ryan Collins <ryan@ohmgeek.co.uk>
2019-01-30 10:10:30 +0000

    Add custom layers, start creating model

--- app/liveness/vox/reconstruction/vrn.py
+++ app/liveness/vox/reconstruction/vrn.py
@@ -3,6 +3,12 @@ This file uses the VRN neural network to convert a 2D image into a 3D voxel stru
 
 This is then passed for classification.
 """
+import numpy as np
+import cv2
+from keras.models import load_model
+
+from liveness.vox.reconstruction import custom_layers
+
 def preprocess_image_for_builder(image, new_size=(192,192)):
     # This takes an image, preprocesses the image.
     ## First, resize to new size (default: 192 x 192)
@@ -20,8 +26,36 @@ def preprocess_image_for_builder(image, new_size=(192,192)):
     return image
 
 class FaceVoxelBuilder(object):
-    def __init__(self, logger):
+    def __init__(self, logger, reconstructor_path=None):
         self._logger = logger
+        
+        # Load in the keras model
+        if(reconstructor_path is None):
+            reconstructor_path = 'vrn-unguided-keras.h5'
+        self._reconstructor_path = reconstructor_path
 
+        # build the model to use later
+        self._model = self._create_builder_model(reconstructor_path)
+
+    def _create_builder_model(self, reconstructor_path):
+        """
+            Create the 3D reconstruction keras model.
+
+            - reconstructor_path: the path to the model on disk
+        """
+
+        custom_objects = {
+            'Conv': custom_layers.Conv,
+            'BatchNorm': custom_layers.BatchNorm,
+            'UpSamplingBilinear': custom_layers.UpSamplingBilinear
+        }
+
+        model = load_model(reconstructor_path, custom_objects=custom_objects)
+
+        return model
+    
     def build_3d(self, image):
+        """
+            Reconstruct a face from 2D -> 3D
+        """
         img = preprocess_image_for_builder(image)

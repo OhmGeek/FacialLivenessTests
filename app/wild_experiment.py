ab392a23bc614a738a66828f67d1398c72745a30
Ryan Collins <ryan@ohmgeek.co.uk>
2019-01-30 10:18:16 +0000

    Refactor old wild experiment code to use our helper

--- app/wild_experiment.py
+++ app/wild_experiment.py
@@ -4,34 +4,38 @@ import visvis as vv
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from liveness.vox.reconstruction.vrn import FaceVoxelBuilder
+import logging
+# custom_objects = {
+#     'Conv': custom_layers.Conv,
+#     'BatchNorm': custom_layers.BatchNorm,
+#     'UpSamplingBilinear': custom_layers.UpSamplingBilinear
+# }
+# model = load_model('vrn-unguided-keras.h5', custom_objects=custom_objects)
+builder = FaceVoxelBuilder(logging.Logger(""))
 
-custom_objects = {
-    'Conv': custom_layers.Conv,
-    'BatchNorm': custom_layers.BatchNorm,
-    'UpSamplingBilinear': custom_layers.UpSamplingBilinear
-}
-model = load_model('vrn-unguided-keras.h5', custom_objects=custom_objects)
 
 img = cv2.imread('/home/ryan/datasets/nuaa/ClientRaw/0001/0001_00_00_02_2.jpg')
-img = cv2.resize(img, (192, 192))
-b,g,r = cv2.split(img)
-img = cv2.merge([r,g,b])
-img = np.swapaxes(img, 2, 0)
-img = np.swapaxes(img, 2, 1)
-img = np.array([img])
+volRGB = builder.build_3d(img)
+# img = cv2.resize(img, (192, 192))
+# b,g,r = cv2.split(img)
+# img = cv2.merge([r,g,b])
+# img = np.swapaxes(img, 2, 0)
+# img = np.swapaxes(img, 2, 1)
+# img = np.array([img])
 
-pred = model.predict(img)
-vol = pred[0] * 255
-im = img[0]
-im = np.swapaxes(im, 0, 1)
-im = np.swapaxes(im, 1, 2)
+# pred = model.predict(img)
+# vol = pred[0] * 255
+# im = img[0]
+# im = np.swapaxes(im, 0, 1)
+# im = np.swapaxes(im, 1, 2)
 
-volRGB = np.stack(((vol > 1) * im[:,:,0],
-                   (vol > 1) * im[:,:,1],
-                   (vol > 1) * im[:,:,2]), axis=3)
-# for i, p in enumerate(volRGB[80:180:5]):
-#     plt.subplot(5, 4, i + 1)
-#     plt.imshow(p)
+# volRGB = np.stack(((vol > 1) * im[:,:,0],
+#                    (vol > 1) * im[:,:,1],
+#                    (vol > 1) * im[:,:,2]), axis=3)
+for i, p in enumerate(volRGB[80:180:5]):
+    plt.subplot(5, 4, i + 1)
+    plt.imshow(p)
 
 
 vv.clf()

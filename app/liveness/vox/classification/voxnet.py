c2f91214a136bd99fdba2772d0c4285cbc38d721
Ryan Collins <ryan@ohmgeek.co.uk>
2019-02-08 10:03:04 +0000

    Create VoxNet

--- app/liveness/vox/classification/voxnet.py
+++ app/liveness/vox/classification/voxnet.py
@@ -1,3 +1,66 @@
 """
 This contains the VoxNet model used to classify the 3d structure.
 """
+
+import keras
+from keras import layers
+from keras.models import Model as keras_Model
+from keras.layers import Dense, Activation, Dropout, Flatten, Conv3D, MaxPooling3D, Lambda
+from keras import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.applications.resnet50 import ResNet50
+from keras.optimizers import Adam
+from keras.engine.input_layer import Input
+from keras.backend import tf
+from liveness.cnn.residual.block import add_common_layers, residual_block
+from liveness.generic import AbstractModel
+import h5py
+
+class ResidualNetwork(AbstractModel):
+    def __init__(self, logger, default_img_dimensions=(224,224), nb_channels=3, cardinality=32):
+        self._img_height = default_img_dimensions[1]
+        self._img_width = default_img_dimensions[0]
+        self._nb_channels = nb_channels
+        self._cardinality = cardinality
+
+        self._model = self._create_model()
+        self._is_model_created = False
+
+        # Now let's go ahead and create the base object.
+        super().__init__(logger)
+
+    def train(self, x, y):
+        self._model.fit(x, y, batch_size=64, epochs=1, verbose=1, validation_split=0.33, shuffle=True)
+
+    def test(self, x, y):
+        score = self._model.evaluate(x, y, verbose=1)
+        return score
+
+    def evaluate(self, image):
+        score = self._model.predict(image)
+        return score
+    
+    # -- Generators: both for fitting and testing.
+    def fit_generator(self, generator, steps_per_epoch=None, epochs=1, shuffle=True, verbose=1, validation_data=None):
+        return self._model.fit_generator(generator, steps_per_epoch=steps_per_epoch, epochs=epochs, shuffle=shuffle, verbose=verbose, validation_data=validation_data)
+
+    def test_generator(self, generator):
+        score = self._model.evaluate_generator(generator, verbose=1, steps=500)
+        return score
+    
+    # -- Override the base.
+    def save(self, pickle_path):
+        self._model.save_weights(pickle_path)
+    
+    def load(self, pickle_path):
+        self._model.load_weights(pickle_path)
+
+    # -- Private functions
+    def _create_model(self, learning_rate=0.001):
+        model = Sequential()
+
+        model.add(Conv3D(32, (5,5,5), strides=(2,2,2), activation='LeakyReLU'))
+        model.add(Conv3D(32, (3,3,3), strides=(1,1,1), activation='LeakyReLU'))
+        model.add(MaxPooling3D(strides=(2,2,2)))
+        model.add(Dense(128))
+        model.add(Dense(2, activation='softmax'))

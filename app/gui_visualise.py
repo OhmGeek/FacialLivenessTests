a89c57ba0860141b23c69f32e4eedccbd7b61492
Ryan Collins <ryan@ohmgeek.co.uk>
2019-02-13 09:43:45 +0000

    initial toby breckon code

---
+++ app/gui_visualise.py
@@ -0,0 +1,42 @@
+#####################################################################
+
+# Example : load and display a set of images from a directory
+# basic illustrative python script
+
+# For use with provided test / training datasets
+
+# Author : Toby Breckon, toby.breckon@durham.ac.uk
+
+# Copyright (c) 2015 / 2016 School of Engineering & Computing Science,
+#                    Durham University, UK
+# License : LGPL - http://www.gnu.org/licenses/lgpl.html
+
+#####################################################################
+
+import cv2
+import os
+
+
+# display all images in directory (sorted by filename)
+
+for filename in sorted(os.listdir(directory_to_cycle)):
+
+    # if it is a PNG file
+
+    if '.png' in filename:
+        print(os.path.join(directory_to_cycle, filename));
+
+        # read it and display in a window
+
+        img = cv2.imread(os.path.join(directory_to_cycle, filename), cv2.IMREAD_COLOR)
+        cv2.imshow('the image',img)
+        key = cv2.waitKey(200) # wait 200ms
+        if (key == ord('x')):
+            break
+
+
+# close all windows
+
+cv2.destroyAllWindows()
+
+#####################################################################
